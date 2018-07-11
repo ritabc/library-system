bd59992cf6421f73e4dd8a4d9c77e4e3b7bd8d49
Craig Wann and Rita Bennett-Chew <rita@>
2018-07-11 14:28:50 -0700

    Pass test for save and == checkout methods

--- lib/Checkout.rb
+++ lib/Checkout.rb
@@ -1,5 +1,5 @@
 class Checkout
-  attr_reader(:book_id, :patron_id, :checkout_date)
+  attr_reader(:book_id, :patron_id, :checkout_date, :id)
   attr_accessor(:due_date)
 
   def initialize(attributes)
@@ -7,5 +7,15 @@ class Checkout
     @patron_id = attributes.fetch(:patron_id)
     @checkout_date = attributes.fetch(:checkout_date) ## :checkout_date and @checkout_date will both be date objects
     @due_date = @checkout_date + 27
+    @id = attributes.fetch(:id)
   end
+  def ==(another_thing)
+    self.book_id().==(another_thing.book_id()).&(self.patron_id().==(another_thing.patron_id())).&(self.checkout_date().==(another_thing.checkout_date()))
+  end
+
+  def save
+    result = DB.exec("INSERT INTO checkouts (book_id, patron_id, checkout_date, due_date) VALUES (#{@book_id}, #{@patron_id}, '#{@checkout_date}', '#{@due_date}' ) RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -11,8 +11,18 @@ require 'spec_helper'
 describe('Checkout') do
   describe('#initialize') do
     it('will test the date methods') do
-      new_checkout = Checkout.new({:book_id => 1, :patron_id => 1, :checkout_date => Date.new(2018, 06, 14)})
+      new_checkout = Checkout.new({:book_id => 1, :patron_id => 1, :checkout_date => Date.new(2018, 06, 14), :id => nil})
       expect(new_checkout.due_date).to(eq(Date.today))
     end
   end
+  describe("#==") do
+    it("are checkout instances the same") do
+      checkout1 = Checkout.new({:book_id => 3, :patron_id => 45, :id => nil, :checkout_date => Date.new(2018, 06, 14)})
+      checkout2 = Checkout.new({:book_id => 3, :patron_id => 45, :id => nil, :checkout_date => Date.new(2018, 06, 14)})
+      checkout1.save
+      checkout2.save
+      expect(checkout1).to(eq(checkout2))
+    end
+  end
+
 end
